d12d33d03f81fdc06570290b7ae282272135a3ad
Paboct <160020049+Paboct@users.noreply.github.com>
2024-11-07 13:21:34 +0100

    Add files via upload
    Fichero inicial sobre los datos

---
+++ data_analysis.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('train_students.csv')
+print(f"Data:\n{data.head()}\n")
+print(f"Stadistical data:\n{data.describe()}\n")
