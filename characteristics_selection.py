2c59630d12c91ce9ac58f0d67c316da49cfd637d
Paboct <pruizm01@estudiantes.unileon.es>
2024-11-27 13:59:06 +0100

    Fichero selección características

---
+++ characteristics_selection.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
+from sklearn.feature_selection import f_classif
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LogisticRegression
+from sklearn.decomposition import PCA
+
